9979e8c413b62ca8f98d5f09185f4aba04b6b328
Kalpesh Santoki <kalpesh.santoki@openxcell.info>
2018-07-28 07:16:14 +0000

    Added port number

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 git pull
-docker build -t hackday-testing:master .
-docker run -d -p 98:80 hackday-testing:master
+branch=$(git symbolic-ref --short HEAD)
+image_name='hackday-testing:'$branch
+c_name='hackday-testing-'$branch
+p_no=`cat port_number`
+# image_name=$brnach$suffix
+echo $image_name
+echo $p_no
+docker rm -f $c_name
+docker rmi -f $image_name
+docker build -t $image_name .
+docker run -d -p $p_no:80 --name $c_name $image_name
